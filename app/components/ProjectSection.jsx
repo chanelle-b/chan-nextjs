4e2183a8ce326885ff20585be6bfda69bb584fdd
Chan <156214630+chanelle-b@users.noreply.github.com>
2024-03-18 22:58:19 -0400

    added projects
    -added projects

--- app/components/ProjectSection.jsx
+++ app/components/ProjectSection.jsx
@@ -34,7 +34,10 @@ const ProjectSection = () => {
                 <br />
               </h1>
               <div class="flex justify-center items-center pr-4">
-                <button class="transition-transform duration-200 scale-150 hover:scale-125 pr-1">
+                <a
+                  href="https://chanfolio.vercel.app"
+                  class="transition-transform duration-200 scale-150 hover:scale-125 pr-1"
+                >
                   <i
                     stroke-linecap="round"
                     stroke-width="2"
@@ -43,8 +46,11 @@ const ProjectSection = () => {
                     viewBox="0 0 24 24"
                     class="bi bi-link-45deg w-8 hover:text-[rgb(99,213,255)]"
                   ></i>
-                </button>
-                <button class="transition-transform duration-200 hover:scale-125">
+                </a>
+                <a
+                  href="https://github.com/chanelle-b/chan-nextjs"
+                  class="transition-transform duration-200 hover:scale-125"
+                >
                   <svg
                     stroke-linejoin="round"
                     stroke-linecap="round"
@@ -56,27 +62,29 @@ const ProjectSection = () => {
                   >
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
-                </button>
+                </a>
               </div>
 
               <p className="text-center text-sm font-light italic pr-2">
-                (In Progress)
+                
               </p>
               <div className="text-sm">
                 <p className="project-body font-medium mt-4 mb-1 text-sm leading-relaxed">
-                  **Describe portfolio here**
+                  Dynamic web portfolio to display various projects, employing
+                  React.js for the UI and Next.js for server-side rendering,
+                  optimizing for performance and SEO.
                 </p>
               </div>
 
               <ul className="project-languages flex flex-wrap -mx-1 pl-10">
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                  React
+                  React.js
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                  SpringBoot
+                  Next.js
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                  PostgreSQL
+                  TailwindCSS
                 </li>
               </ul>
             </div>
@@ -92,7 +100,10 @@ const ProjectSection = () => {
                 <br />
               </h1>
               <div class="flex justify-center items-center">
-                <button class="transition-transform duration-200 hover:scale-125">
+                <a
+                  href="https://github.com/chanelle-b/Screen-Seeker"
+                  class="transition-transform duration-200 hover:scale-125"
+                >
                   <svg
                     stroke-linejoin="round"
                     stroke-linecap="round"
@@ -104,27 +115,29 @@ const ProjectSection = () => {
                   >
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
-                </button>
+                </a>
               </div>
 
               <p className="text-center text-sm font-light italic">
                 (In Progress)
               </p>
               <div className="text-slate-300 hover:text-purple-300 text-sm">
-                <p className="project-body font-medium mt-4 mb-1 text-sm leading-relaxed">
-                  **Describe Project Here**
+                <p className="project-body font-medium mt-1 mb-1 text-sm leading-relaxed">
+                  Developing a responsive movie exploration application using
+                  React.js, integrating TailwindCSS for a mobile-first, adaptive
+                  design, enhancing user experience across various devices.
                 </p>
               </div>
 
-              <ul className="project-languages flex flex-wrap -mx-1 pl-10">
+              <ul className="project-languages flex flex-wrap -mx-1 pl-8">
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  React
+                  React.js
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  SpringBoot
+                  JavaScript
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  PostgreSQL
+                  TailwindCSS
                 </li>
               </ul>
             </div>
@@ -137,11 +150,11 @@ const ProjectSection = () => {
             <div className="card gradient-border p-0.2 inline-block">
               <div className="card2 content border border-purple-400 p-4 text-white hover:text-black bg-black text-transparent bg-clip-text bg-gradient-to-r from-purple-900 via-blue-400 to-pink-500">
                 <h1 className="project-title text-2xl font-semibold scale-125 drop-shadow-md pb-1 pt-2">
-                  **Project Name**
+                  Party Manager
                   <br />
                 </h1>
                 <div class="flex justify-center items-center">
-                  <button class="transition-transform duration-200 hover:scale-125">
+                  <a href="https://github.com/chanelle-b/Party-Manager" class="transition-transform duration-200 hover:scale-125">
                     <svg
                       stroke-linejoin="round"
                       stroke-linecap="round"
@@ -153,25 +166,27 @@ const ProjectSection = () => {
                     >
                       <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                     </svg>
-                  </button>
+                  </a>
                 </div>
                 <p className="text-center text-sm font-light italic">
-                  (In Progress)
+                  
                 </p>
                 <div className="text-sm">
                   <p className="project-body font-medium mt-4 mb-1 text-sm leading-relaxed">
-                    **Describe Project Here**
+                    Developed an integrated party manager system using C#
+                    ASP.NETMVC for backend operations, coupled with a bootstrap
+                    frontend for aresponsive and intuitive user interface.
                   </p>
                 </div>
-                <ul className="project-languages flex flex-wrap -mx-1 pl-10">
+                <ul className="project-languages flex flex-wrap -mx-1 pl-14">
                   <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                    React
+                    C#
                   </li>
                   <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                    SpringBoot
+                    ASP.NET MVC
                   </li>
                   <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-white text-black text-xs">
-                    PostgreSQL
+                    Bootstrap
                   </li>
                 </ul>
               </div>
@@ -187,11 +202,11 @@ const ProjectSection = () => {
           <div className="card gradient-border p-0.2 inline-block -mt-">
             <div className="card2 content border border-purple-400 p-4 text-white hover:text-purple-300">
               <h1 className="project-title text-2xl font-semibold scale-125 drop-shadow-md pb-1 pt-2">
-                **Project Name**
+                AI ChatBot
                 <br />
               </h1>
               <div class="flex justify-center items-center">
-                <button class="transition-transform duration-200 hover:scale-125">
+                <a href="https://github.com/chanelle-b/AI-Chatbot" class="transition-transform duration-200 hover:scale-125">
                   <svg
                     stroke-linejoin="round"
                     stroke-linecap="round"
@@ -203,7 +218,7 @@ const ProjectSection = () => {
                   >
                     <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
-                </button>
+                </a>
               </div>
 
               <p className="text-center text-sm font-light italic">
@@ -211,19 +226,19 @@ const ProjectSection = () => {
               </p>
               <div className="text-slate-300 hover:text-purple-300 text-sm">
                 <p className="project-body font-medium mt-4 mb-1 text-sm leading-relaxed">
-                  **Describe Project Here**
+                  Currently developing an AI Chatbot
                 </p>
               </div>
 
-              <ul className="project-languages flex flex-wrap -mx-1 pl-10">
+              <ul className="project-languages flex flex-wrap -mx-1 pl-16">
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  React
+                  Node.js
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  SpringBoot
+                  Python
                 </li>
                 <li className="font-mono mx-1 my-1 px-2 py-1 rounded-md bg-opacity-50 bg-[#b37cf1] text-black text-xs">
-                  PostgreSQL
+                  *****
                 </li>
               </ul>
             </div>
